b0bbfe6dd5a94388b5000f24558feba55827202d
Mythical <MythicalData@gmail.com>
2024-06-02 02:29:23 -0400

    cleanup

--- src/__main__.py
+++ src/__main__.py
@@ -1,26 +1,25 @@
-import threads as threads
+import thread_constant
+import thread_game_monitor
 from packing import make_mods
 from game_runner import run_game
 from enums import ScriptStateType
-from script_states import ScriptState, is_script_state_used
+from script_states import ScriptState
+from utilities import toggle_engine_off, toggle_engine_on, clean_working_dir
+
 
 
 def main():
     ScriptState.set_script_state(ScriptStateType.INIT)
-    threads.constant_thread()
+    thread_constant.constant_thread()
     ScriptState.set_script_state(ScriptStateType.POST_INIT)
+    clean_working_dir()
     make_mods()
-    import utilities
-    if utilities.is_toggle_engine_during_testing_in_use():
-        utilities.close_game_engine()
+    toggle_engine_off()
     run_game()
-    threads.game_moniter_thread()
-    if utilities.is_toggle_engine_during_testing_in_use():
-        utilities.open_game_engine()
-        from engine_moniter import engine_moniter_thread
-        engine_moniter_thread()
-    if is_script_state_used(ScriptStateType.CONSTANT):
-        threads.stop_constant_thread()
+    thread_game_monitor.game_moniter_thread()
+    toggle_engine_on()
+    thread_constant.stop_constant_thread()
+    clean_working_dir()
 
 
 if __name__ == '__main__':

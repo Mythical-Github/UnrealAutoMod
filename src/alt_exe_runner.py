f9eb40571b0a5c27ae82bc0b42da6abb710dcb8d
Mythical <MythicalData@gmail.com>
2024-06-02 02:28:53 -0400

    cleanup

--- src/alt_exe_runner.py
+++ src/alt_exe_runner.py
@@ -11,6 +11,5 @@ def alt_exe_checks(script_state_type: ScriptStateType):
         if exe_state == script_state_type:
             exe_path = alt_exe_method['alt_exe_path']
             exe_args = alt_exe_method['variable_args']
-            exe_exec_mode = alt_exe_method['execution_mode']
-            exe_exec_mode = get_enum_from_val(ExecutionMode, exe_exec_mode)
+            exe_exec_mode = get_enum_from_val(ExecutionMode, alt_exe_method['execution_mode'])
             run_app(exe_path, exe_exec_mode, exe_args)

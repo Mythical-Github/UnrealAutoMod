1df7866845aed4d78fd8bcf351250203d5193cac
Mythical <MythicalData@gmail.com>
2024-06-03 04:55:49 -0400

    added logic for skipping game launch

--- src/__main__.py
+++ src/__main__.py
@@ -4,8 +4,7 @@ from packing import make_mods
 from game_runner import run_game
 from enums import ScriptStateType
 from script_states import ScriptState
-from utilities import toggle_engine_off, toggle_engine_on, clean_working_dir
-
+from utilities import toggle_engine_off, toggle_engine_on, clean_working_dir, get_skip_launching_game
 
 
 def main():
@@ -13,11 +12,12 @@ def main():
     thread_constant.constant_thread()
     ScriptState.set_script_state(ScriptStateType.POST_INIT)
     clean_working_dir()
-    make_mods()
+    make_mods()    
     toggle_engine_off()
-    run_game()
-    thread_game_monitor.game_moniter_thread()
-    toggle_engine_on()
+    if not get_skip_launching_game():   
+        run_game()
+        thread_game_monitor.game_moniter_thread()
+        toggle_engine_on()
     thread_constant.stop_constant_thread()
     clean_working_dir()
 

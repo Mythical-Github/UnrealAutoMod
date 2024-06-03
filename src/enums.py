d73b9f0eca922e1e7f093dcc930269d175475ffb
Mythical <MythicalData@gmail.com>
2024-06-03 05:33:43 -0400

    .

--- src/enums.py
+++ src/enums.py
@@ -46,11 +46,11 @@ class ScriptStateType(Enum):
     POST_GAME_LAUNCH = 'post_game_launch'
     PRE_GAME_CLOSE = 'pre_game_close'
     POST_GAME_CLOSE = 'post_game_close'
-    PRE_ENGINE_CLOSE = 'pre_engine_close'
-    POST_ENGINE_CLOSE = 'post_engine_close'
     PRE_ENGINE_OPEN = 'pre_engine_open'
     POST_ENGINE_OPEN = 'post_engine_open'
-    
+    PRE_ENGINE_CLOSE = 'pre_engine_close'
+    POST_ENGINE_CLOSE = 'post_engine_close'
+
 
 class ExecutionMode(Enum):
     SYNC = 'sync'

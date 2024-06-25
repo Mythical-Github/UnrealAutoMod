87182e71d32a0fbf6cec48f018482304919ad019
Mythical <MythicalData@gmail.com>
2024-06-24 22:32:46 -0400

    cyclic import cleanup

---
+++ src/mods.py
@@ -0,0 +1,23 @@
+import enums
+import engine
+import packing
+import utilities
+import game_runner
+import script_states
+import thread_constant
+import thread_game_monitor
+
+
+def create_mods():
+    script_states.ScriptState.set_script_state(enums.ScriptStateType.INIT)
+    thread_constant.constant_thread()
+    script_states.ScriptState.set_script_state(enums.ScriptStateType.POST_INIT)
+    utilities.clean_working_dir()
+    packing.make_mods()
+    engine.toggle_engine_off()
+    if not utilities.get_skip_launching_game():
+        game_runner.run_game()
+        thread_game_monitor.game_moniter_thread()
+        engine.toggle_engine_on()
+    thread_constant.stop_constant_thread()
+    utilities.clean_working_dir()

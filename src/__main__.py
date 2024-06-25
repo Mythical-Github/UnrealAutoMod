28552878c8a84f2406377390544d7730a1ee11de
Mythical <MythicalData@gmail.com>
2024-06-24 22:20:37 -0400

    cleanup

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,8 @@
 import cli
-import log
 
 
 if __name__ == "__main__":
     try:
         cli.cli_logic()
     except Exception as error_message:
-        log.log_message(error_message)
+        cli.log.log_message(error_message)

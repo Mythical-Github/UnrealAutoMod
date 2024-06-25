c3ca92e62dd67fe4f7fcb60da6eaa660f1ee1255
Mythical <MythicalData@gmail.com>
2024-06-24 22:24:23 -0400

    Revert "cleanup"
    This reverts commit 28552878c8a84f2406377390544d7730a1ee11de.

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,9 @@
 import cli
+import log
 
 
 if __name__ == "__main__":
     try:
         cli.cli_logic()
     except Exception as error_message:
-        cli.log.log_message(error_message)
+        log.log_message(error_message)

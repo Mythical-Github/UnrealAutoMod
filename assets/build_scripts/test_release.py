d2191941addc0af2443473740bf4e27bf66c007c
Mythical <MythicalData@gmail.com>
2024-07-29 23:58:44 -0400

    .

--- assets/build_scripts/test_release.py
+++ assets/build_scripts/test_release.py
@@ -10,7 +10,10 @@ os.chdir(SCRIPT_DIR)
 assets_dir = os.path.dirname(SCRIPT_DIR)
 run_bat_file = f'{assets_dir}/base/run.bat'
 if os.path.isfile(run_bat_file):
+    os.system('cls')
     os.system(run_bat_file)
+
+sys.exit()
 
 # help_bat_file = f'{assets_dir}/base/help.bat'
 # if os.path.isfile(help_bat_file):

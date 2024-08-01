841182f391b8979727b7cc633c4de23b28ffa29f
Mythical <MythicalData@gmail.com>
2024-08-01 00:17:22 -0400

    fixed bug in tetsing scripts

--- assets/build_scripts/test_release.py
+++ assets/build_scripts/test_release.py
@@ -8,7 +8,7 @@ else:
 os.chdir(SCRIPT_DIR)
 
 assets_dir = os.path.dirname(SCRIPT_DIR)
-run_bat_file = f'{assets_dir}/base/run.bat'
+run_bat_file = f'{assets_dir}/base/run_default.bat'
 if os.path.isfile(run_bat_file):
     os.system('cls')
     os.system(run_bat_file)

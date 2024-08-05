9df51036b63c1695cc547e32dd2bbc4c63bf9963
Mythical-Github <MythicalData@gmail.com>
2024-08-05 01:37:09 -0400

    cleanup

--- src/game_runner.py
+++ src/game_runner.py
@@ -1,7 +1,42 @@
+import winreg
+
 import script_states
-import steam
 import utilities
 from enums import ScriptStateType, GameLaunchType, ExecutionMode
+
+
+def get_steam_exe_location():
+    try:
+        reg_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\WOW6432Node\Valve\Steam", 0, winreg.KEY_READ)
+        install_path, _ = winreg.QueryValueEx(reg_key, "InstallPath")
+        winreg.CloseKey(reg_key)
+        steam_exe_path = f"{install_path}\\steam.exe"
+        return steam_exe_path
+    except FileNotFoundError:
+        return "Steam: installation not found in the registry."
+    except Exception as e:
+        return f"Error: An error occurred: {e}"
+
+
+def get_epic_launcher_exe_location():
+    first_arg = winreg.HKEY_LOCAL_MACHINE
+    second_arg = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+    reg_key = winreg.OpenKey(first_arg, second_arg, 0, winreg.KEY_READ | winreg.KEY_WOW64_32KEY)
+    for i in range(winreg.QueryInfoKey(reg_key)[0]):
+        sub_key_name = winreg.EnumKey(reg_key, i)
+        sub_key = winreg.OpenKey(reg_key, sub_key_name)
+        try:
+            display_name = winreg.QueryValueEx(sub_key, "DisplayName")[0]
+            if "Epic Games Launcher" in display_name:
+                install_path, _ = winreg.QueryValueEx(sub_key, "InstallLocation")
+                epic_launcher_exe_path = f"{install_path}\\Portal\\Binaries\\Win32\\EpicGamesLauncher.exe"
+                return epic_launcher_exe_path
+        except FileNotFoundError:
+            pass
+        finally:
+            winreg.CloseKey(sub_key)
+    winreg.CloseKey(reg_key)
+    return "Epic Games Launcher installation not found in the registry."
 
 
 def run_game_exe():
@@ -12,7 +47,7 @@ def run_game_steam():
     if utilities.get_override_automatic_launcher_exe_finding():
         steam_exe = utilities.get_game_launcher_exe_path()
     else:
-        steam_exe = steam.get_steam_exe_location()
+        steam_exe = get_steam_exe_location()
     launch_params = []
     launch_params.append('-applaunch')
     launch_params.append(utilities.get_game_id())

3bc66a2c40a97483cc9cdf59a02ff573d4825fcc
Mythical-Github <MythicalData@gmail.com>
2024-08-08 21:23:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='unreal_auto_mod',
+    name='UnrealAutoMod',
     version='0.1',
     packages=find_packages(),
     install_requires=[

65c55ad0dfa96f4967d458aea3f0e20990c2d536
Mythical-Github <MythicalData@gmail.com>
2024-08-08 23:30:26 -0400

    fixes

--- src/unreal_auto_mod/__main__.py
+++ src/unreal_auto_mod/__main__.py
@@ -3,7 +3,8 @@ import __init__
 import sys
 from pathlib import Path
 
-from cli_py import cli_py as cli
+import cli_py.cli_py
+import log_py
 from log_py import log_py as log
 
 
@@ -17,7 +18,7 @@ if __name__ == "__main__":
     try:
         log.set_log_base_dir(script_dir)
         log.set_colors_json_path(f'{script_dir}/json/log_colors.json')
-        cli.set_json_location(f'{script_dir}/json/cli.json')
-        cli.cli_logic()
+        cli_py.cli_py.set_json_location(f'{script_dir}/json/cli.json')
+        cli_py.cli_py.cli_logic()
     except Exception as error_message:
         log.log_message(str(error_message))     

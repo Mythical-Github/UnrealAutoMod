b2d6147282aa4a798ca280d4a1f0dcdd8c81e89d
Mythical-Github <MythicalData@gmail.com>
2024-08-08 23:53:59 -0400

    fix

--- src/unreal_auto_mod/__main__.py
+++ src/unreal_auto_mod/__main__.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     try:
         log.set_log_base_dir(script_dir)
         log.set_colors_json_path(f'{script_dir}/json/log_colors.json')
-        cli_py.cli_py.set_json_location(f'{script_dir}/json/cli.json')
-        cli_py.cli_py.cli_logic()
+        cli_py.set_json_location(f'{script_dir}/json/cli.json')
+        cli_py.cli_logic()
     except Exception as error_message:
         log.log_message(str(error_message))     

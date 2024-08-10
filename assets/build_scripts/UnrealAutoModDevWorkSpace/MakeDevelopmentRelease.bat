2fc36011b27a99ee0340071873b9ba6243bdd5e6
Mythical-Github <MythicalData@gmail.com>
2024-08-10 05:14:49 -0400

    updated build scripts

---
+++ assets/build_scripts/UnrealAutoModDevWorkSpace/MakeDevelopmentRelease.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set "UnrealAutoModDir=%~dp0UnrealAutoMod"
+set "DevelopmentDirToZip=%UnrealAutoModDir%\assets\build_scripts\UnrealAutoModDevWorkSpace"
+set "FinalZipLocation=%~dp0Development.zip"
+
+cd /d "%UnrealAutoModDir%"
+
+echo Creating zip file from %DevelopmentDirToZip% to %FinalZipLocation%
+powershell -Command "Compress-Archive -Path '%DevelopmentDirToZip%' -DestinationPath '%FinalZipLocation%' -Force"
+
+pause
+
+exit /b 0

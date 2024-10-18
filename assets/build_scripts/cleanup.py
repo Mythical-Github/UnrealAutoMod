27f1c1df87e8d9dcc35c9b0ae6bd1cb4e4654b29
Mythical-Github <MythicalData@gmail.com>
2024-10-18 04:36:57 -0400

    Update cleanup.py

--- assets/build_scripts/cleanup.py
+++ assets/build_scripts/cleanup.py
@@ -3,16 +3,11 @@ import sys
 import shutil
 
 
-# delete old zips
-# delete old log dirs
-# "C:\Users\Mythical\Documents\GitHub\UnrealAutoModDevWorkSpace\UnrealAutoMod\assets\base\UnrealAutoMod.zip"
-# "C:\Users\Mythical\Documents\GitHub\UnrealAutoModDevWorkSpace\UnrealAutoMod\assets\base\logs"
-
-
 if getattr(sys, 'frozen', False):
     SCRIPT_DIR = os.path.dirname(os.path.abspath(sys.executable))
 else:
     SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+    
 os.chdir(SCRIPT_DIR)
 
 BASE_DIR = os.path.normpath(os.path.join(SCRIPT_DIR, '..', 'base'))

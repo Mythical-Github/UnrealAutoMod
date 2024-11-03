b44f88ca4fe6f402fa79178681f3c41679448d4b
Mythical-Github <MythicalData@gmail.com>
2024-11-03 15:25:22 -0500

    cleanup

--- src/unreal_auto_mod/__init__.py
+++ src/unreal_auto_mod/__init__.py
@@ -1,15 +1,1 @@
 from unreal_auto_mod._version import __version__, __version_tuple__
-
-# import pygetwindow as gw
-
-# TERMINAL_TITLE = "UnrealAutoMod Automation Script"
-
-# def close_terminals_by_title(title):
-#     for window in gw.getWindowsWithTitle(title):
-#         try:
-#             window.close()
-#             print(f"Closed window with title: {title}")
-#         except Exception as e:
-#             print(f"Error closing window: {e}")
-
-# close_terminals_by_title(TERMINAL_TITLE)

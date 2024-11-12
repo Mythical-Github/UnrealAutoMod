49fb82dac765fe8d0ada8b4079888b6ded4acfca
Mythical-Github <MythicalData@gmail.com>
2024-11-12 17:29:56 -0500

    Update unreal_auto_mod_updater.bat

--- assets/base/unreal_auto_mod_updater.bat
+++ assets/base/unreal_auto_mod_updater.bat
@@ -4,7 +4,7 @@ cd /d %~dp0
 
 set "project_directory=%CD%"
 set "content_zip_urls=https://github.com/Mythical-Github/unreal_auto_mod/releases/latest/download/unreal_auto_mod.zip"
-set "project_updater_exe=%CD%/project_updater/project_updater.exe"
+set "project_updater_exe=%CD%\project_updater\project_updater.exe"
 
 set "command=update_project --project_directory "%project_directory%" --content_zip_urls "%content_zip_urls%" --backup_exclusions project_updater unreal_auto_mod_updater.bat"
 

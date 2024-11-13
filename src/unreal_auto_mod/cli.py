ce51464ee79f98d176b08f1dd4bf9b9239ee46ec
Mythical-Github <MythicalData@gmail.com>
2024-11-13 05:06:07 -0500

    Revert "updated cli setup to use argparse"
    This reverts commit 96237eac39d17181c7eeb779960ee2d3f4e62afa.

--- src/unreal_auto_mod/cli.py
+++ src/unreal_auto_mod/cli.py
@@ -6,106 +6,61 @@ OPTIONS = {
         "test_mods": {
             "function_name": "test_mods",
             "arg_help_pairs": [
-                {"settings_json_path": {
-                    "help": "Path to settings.json",
-                    "required": True,
-                    "use_nargs": False
-                }},
-                {"mod_names": {
-                    "help": "list of mod_names, strings",
-                    "required": True,
-                    "use_nargs": True
-                }}
+                {"mod_names": "list of mod_names, strings"}
             ]
         },
         "test_mods_all": {
             "function_name": "test_mods_all",
             "arg_help_pairs": [
-                {"settings_json_path": {
-                    "help": "Path to settings.json",
-                    "required": True,
-                    "use_nargs": False
-                }}
+                {"settings_json_path": "Path to settings.json"}
             ]
         },
         "open_latest_log": {
             "function_name": "open_latest_log",
             "arg_help_pairs": [
-                {"settings_json_path": {
-                    "help": "Path to settings.json",
-                    "required": True,
-                    "use_nargs": False
-                }}
+                {"settings_json_path": "Path to settings.json"}
             ]
         },
         "run_game": {
             "function_name": "run_game",
             "arg_help_pairs": [
-                {"settings_json_path": {
-                    "help": "Path to settings.json",
-                    "required": True,
-                    "use_nargs": False
-                }}
+                {"settings_json_path": "Path to settings.json"}
             ]
         },
         "install_uasset_gui": {
             "function_name": "open_uasset_gui",
             "arg_help_pairs": [
-                {"settings_json_path": {
-                    "help": "Path to settings.json",
-                    "required": True,
-                    "use_nargs": False
-                }}
+                {"settings_json_path": "Path to settings.json"}
             ]
         },
         "install_kismet_analyzer": {
             "function_name": "open_kismet_analyzer",
             "arg_help_pairs": [
-                {"settings_json_path": {
-                    "help": "Path to settings.json",
-                    "required": True,
-                    "use_nargs": False
-                }}
+                {"settings_json_path": "Path to settings.json"}
             ]
         },
         "install_stove": {
             "function_name": "open_stove",
             "arg_help_pairs": [
-                {"settings_json_path": {
-                    "help": "Path to settings.json",
-                    "required": True,
-                    "use_nargs": False
-                }}
+                {"settings_json_path": "Path to settings.json"}
             ]
         },
         "install_umodel": {
             "function_name": "open_umodel",
             "arg_help_pairs": [
-                {"settings_json_path": {
-                    "help": "Path to settings.json",
-                    "required": True,
-                    "use_nargs": False
-                }}
+                {"settings_json_path": "Path to settings.json"}
             ]
         },
         "install_fmodel": {
             "function_name": "open_fmodel",
             "arg_help_pairs": [
-                {"settings_json_path": {
-                    "help": "Path to settings.json",
-                    "required": True,
-                    "use_nargs": False
-                }}
+                {"settings_json_path": "Path to settings.json"}
             ]
         },
         "install_spaghetti": {
             "function_name": "open_spaghetti",
             "arg_help_pairs": [
-                {"settings_json_path": {
-                    "help": "Path to settings.json",
-                    "required": True,
-                    "use_nargs": False
-                }}
+                {"settings_json_path": "Path to settings.json"}
             ]
         }
     }

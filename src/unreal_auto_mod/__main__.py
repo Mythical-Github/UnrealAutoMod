75a786677fff50c9d0851677f12ee2f6fbb55122
Mythical-Github <MythicalData@gmail.com>
2024-11-13 03:12:52 -0500

    switch from colorama to rich

--- src/unreal_auto_mod/__main__.py
+++ src/unreal_auto_mod/__main__.py
@@ -4,21 +4,21 @@ from pathlib import Path
 from unreal_auto_mod import cli_py
 from unreal_auto_mod import log_py as log
 from unreal_auto_mod.cli import OPTIONS
-from unreal_auto_mod.log_colors import COLORS
 
 
 if getattr(sys, 'frozen', False):
-    script_dir = Path(sys.executable).parent
+    SCRIPT_DIR = Path(sys.executable).parent
 else:
-    script_dir = Path(__file__).resolve().parent
+    SCRIPT_DIR = Path(__file__).resolve().parent
+
 
 def main():
     try:
-        log.set_log_base_dir(script_dir)
-        log.configure_logging(COLORS)
+        log.set_log_base_dir(SCRIPT_DIR)
         cli_py.cli_logic(OPTIONS)
     except Exception as error_message:
         log.log_message(str(error_message))
+
 
 if __name__ == "__main__":
     main()
